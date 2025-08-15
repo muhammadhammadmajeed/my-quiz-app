9f9678324da6437e03ec5ab8259a8e24575297c8
muhammadhammadmajeed <mhm.developerhub@gmail.com>
2025-08-15 21:33:09 +0500

    update packaages

--- src/App.js
+++ src/App.js
@@ -3,34 +3,7 @@ import './App.css';
 import React from 'react'
 
 export default function App() {
-  let questions = [
-    {
-      statement: "What is the name of capital of Pakistan?",
-      options: ['Lahore', 'Karachi', 'Islamabad', 'Peshawar'],
-      ans: 'Islamabad',
-    },
-    {
-      statement: "What is the name of capital of Turkey?",
-      options: ['Ankara', 'Istanbul', 'Izmir', 'Bursa'],
-      ans: 'Ankara',
-    },
-    {
-      statement: "What is the name of capital of United Arab Emirates?",
-      options: ['Sharjah', 'Abu Dhabi', 'Dubai', 'Ajman'],
-      ans: 'Abu Dhabi',
-    },
-    {
-      statement: "What is the name of the capital of Saudi Arabia?",
-      options: ['Jeddah', 'Makkah', 'Dammam', 'Riyadh'],
-      ans: 'Riyadh',
-    }, {
-      statement: "What is the name of the capital of Qatar?",
-      options: ['Doha', 'Al Rayyan', 'Lusail', 'Al Wakrah'],
-      ans: 'Doha',
-    },
-
-
-  ]
+   
   return (
     <div className='container my-5'>
       <h1 style={{ textAlign: "center", color: "red" }}>
@@ -41,11 +14,22 @@ export default function App() {
           return (
             <>
               <div >
-                <li  className="bg-secondary" >{q.statement}</li>
+                <li className="bg-secondary" >{q.statement}</li>
                 <ul className='text-primary'>
                   {
-                    q.options.map((op)=> <li className='text-primary'>{op}</li>)
-                  }   
+                    q.options.map((op) => {
+                      return(
+                        <>
+                      <input type="radio" id='html' />
+                      <label htmlFor="html">
+                      <li className='text-primary'>{op}</li>
+
+                      </label>
+                      </>
+                    )
+                    }
+                  )
+                  }
                 </ul>
               </div>
 
